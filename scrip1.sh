f381565aaff4a91b42140e2517e2b64b94f07d2e
thma4828 <thma4828@colorado.edu>
2018-09-05 19:15:53 -0400

    commit 10

--- scrip1.sh
+++ scrip1.sh
@@ -7,12 +7,15 @@ echo "----USERS AND SHELLS----"
 var3=$(cat /etc/passwd | wc -l)
 let "var3 = var3 - 1"
 echo "Number of users: ${var3}"
-var4=$(ps -ef | grep -e "bash" | wc -l)
+var4=$(ps -ef | grep "bash" | wc -l)
 echo "Number of bash shells: ${var4}"
-var5=$(ps -ef | grep -e "false" | wc -l)
+var5=$(ps -ef | grep "false" | wc -l)
 echo "Number of false shells: ${var5}"
-var6=$(ps -ef | grep -e "nologin" | wc -l)
+var6=$(ps -ef | grep "nologin" | wc -l)
 echo "Number of nologin shells: ${var6}"
 echo "----FILE SYSTEMs / DIRECTORIES----"
-var7=$(find /../.. -type f | wc -l)
+var7=$(find .. /../.. -type f | wc -l)
 echo "Number of files in the system: ${var7}"
+var8=$(find ../../.. -type d | wc -l)
+echo "Number of directories in the system: ${var8}"
+
