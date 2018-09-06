9b1913fd6f0c533741be518ac6d9657f2e1c6812
thma4828 <thma4828@colorado.edu>
2018-09-05 21:23:47 -0400

    changes to scrip2.sh

--- scrip2.sh
+++ scrip2.sh
@@ -5,11 +5,19 @@ echo "slash dir at: ${v1}% || slash-boot dir at: ${v2}%"
 if [ $v1 -gt 80 ]
 	then
 		echo "the root directory has reached 80%, email being sent to root"
+		touch tmfile.txt
+		echo "root dir near full" >>tmfile.txt
+		mail -s "dear root" theodore.margoles@colorado.edu < tmfile.txt
+		rm tmfile.txt
 fi 
 
 if [ $v2 -gt 80 ]
 	then
 		echo "the boot directory has reached 80%, email being sent to root"
+		touch tm2.txt
+		echo "boot dir near full" >>tm2.txt
+		mail -s "dear root" theodore.margoles@colorado.edu < tm2.txt
+		rm tm2.txt
 fi
 
 
