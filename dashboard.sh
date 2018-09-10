15c6c2d25e03e2004d6fcd95af09853e7f36112c
thma4828 <thma4828@colorado.edu>
2018-09-10 08:02:21 -0400

    almost done with dashboard

--- dashboard.sh
+++ dashboard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "----CPU/RAM USAGE----"
-var1=$(free -m | cut -d' ' -f31 | tail -n 2 | grep . )
+var1=$(free -m | grep Mem | cut -d' ' -f31)
 echo  "Free RAM: ${var1} MB"
 var2=$(uptime | cut -d: -f4)
 echo "CPU load average: ${var2}"
@@ -8,16 +8,16 @@ echo "----USERS AND SHELLS----"
 var3=$(cat /etc/passwd | wc -l)
 let "var3 = var3 - 1"
 echo "Number of users: ${var3}"
-var4=$(ps -ef | grep "bash" | wc -l)
+var4=$(cat /etc/passwd | grep bash | wc -l)
 echo "Number of bash shells: ${var4}"
-var5=$(ps -ef | grep "false" | wc -l)
+var5=$(cat /etc/passwd | grep false | wc -l)
 echo "Number of false shells: ${var5}"
-var6=$(ps -ef | grep "nologin" | wc -l)
+var6=$(cat /etc/passwd | grep nologin | wc -l)
 echo "Number of nologin shells: ${var6}"
 echo "----FILE SYSTEMs / DIRECTORIES----"
-var7=$(find .. /../.. -type f | wc -l)
+var7=$(find / -type f | wc -l)
 echo "Number of files in the system: ${var7}"
-var8=$(find ../../.. -type d | wc -l)
+var8=$(find / -type d | wc -l)
 echo "Number of directories in the system: ${var8}"
 echo "----NETWORK----"
 var9=$(ping -w 1 127.0.0.1 | grep -e "packet loss" | cut -d, -f3 | cut -b 2)
@@ -30,14 +30,14 @@ if [ $var9 -gt 0 ]
 		echo "connectivity failure!"
 fi
 echo -n "enp0s3 Bytes Recieved: "
-var10=$(cat /proc/net/dev | cut -b 10,11,12,13,14,15,16,17,18,19 | sort -nr | head -n 1 | cut -d' ' -f2)
+var10=$(cat /proc/net/dev | grep enp0s3 | cut -d: -f2 | cut -d' ' -f4)
 echo -n "$var10"
 echo -n "        enp0s3 Bytes Transmitted: "
 varBT=$(cat /proc/net/dev | grep enp0s3 | cut -d' ' -f49)
 echo "$varBT"
-varLO=$(cat /proc/net/dev | cut -d' ' -f9 | tail -n 1)
+varLO=$(cat /proc/net/dev | grep lo | cut -d: -f2 | cut -d' ' -f5)
 echo -n "lo Bytes Recieved: $varLO"
-varLT=$(cat /proc/net/dev | grep lo | cut -d' ' -f56)
+varLT=$(cat /proc/net/dev | grep lo | cut -d: -f2 | cut -d' ' -f51)
 echo "     lo Bytes Transmitted: $varLT"
 
 
