4182c134746ceb175a60eca4975c8c3817e5ce1b
thma4828 <thma4828@colorado.edu>
2018-09-12 01:43:02 -0400

    h

--- mon.sh
+++ mon.sh
@@ -2,21 +2,21 @@
 v1=$(df / | cut -d' ' -f10 | grep . | cut -b 1,2)
 v2=$(df /boot | cut -d' ' -f16 | grep . | cut -b 1,2)
 echo "slash dir at: ${v1}% || slash-boot dir at: ${v2}%"
-if [ $v1 -gt 80 ]
+if [ $v1 -gt 5 ]
 	then
 		echo "the root directory has reached 80%, email being sent to root"
 		touch tmfile.txt
 		echo "root dir near full" >>tmfile.txt
-		mail -s "dear sys admin" theodore.margoles@colorado.edu < tmfile.txt
+		mail -s "dear sys admin" root < tmfile.txt
 		rm tmfile.txt
 fi 
 
-if [ $v2 -gt 80 ]
+if [ $v2 -gt 5 ]
 	then
 		echo "the boot directory has reached 80%, email being sent to root"
 		touch tm2.txt
 		echo "boot dir near full" >>tm2.txt
-		mail -s "dear sys admin" theodore.margoles@colorado.edu < tm2.txt
+		mail -s "dear sys admin" root < tm2.txt
 		rm tm2.txt
 fi
 
